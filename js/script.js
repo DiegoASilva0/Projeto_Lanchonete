24aa8cdd8a235fc3f48aad326fe9ad25692104da
Diego A Silva <diegoestudante0@gmail.com>
2026-08-19 18:48:25 -0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+function clickmenu() {
+  let menu = document.getElementById("menu-mobile");
+
+  if (menu.style.display == "block") {
+    menu.style.display = "none";
+  } else {
+    menu.style.display = "block";
+  }
+}
+
+function fecharMenu() {
+  let menu = document.getElementById("menu-mobile");
+
+  if (window.innerWidth >= 825) {
+    menu.style.display = "block";
+  } else {
+    menu.style.display = "none";
+  }
+}
